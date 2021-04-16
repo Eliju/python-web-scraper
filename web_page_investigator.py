355d0e5a911f5cb762f27461ba26def7a76e7e68
Eliju <36505898+Eliju@users.noreply.github.com>
2021-04-16 23:19:30 +0300

    More elements searched using regex

--- web_page_investigator.py
+++ web_page_investigator.py
@@ -10,13 +10,23 @@ def open_url():
 
     titles = get_titles(html)
     years = get_years(html)
-    print(html)
-    print(len(titles))
-    print(len(years))
+    genres = get_genres(html)
+    descriptions = get_descriptions(html)
+    images = get_images(html)
+    ranks = get_ranks(html)
+    directors = get_director(html)
+    stars = get_stars(html)
+    classification_details = get_classification_details(html)
+    rating_values = get_rating_values(html)
+    metascores = get_metascores(html)
+    #print(html)
+    #print(len(classification_details))
+    print(metascores)
+    print(len(metascores))
 
 
 def get_titles(html):
-    pattern = "<a href=./title/tt.*?/.\n>.*?</a>"
+    pattern = "<h3 class=.*?<a href=./title/tt.*?/.\n>.*?</a>"
     return get_elements(html, pattern)
 
 
@@ -25,7 +35,59 @@ def get_years(html):
     return get_elements(html, pattern)
 
 
-def get_elements(html, pattern):
+def get_genres(html):
+    pattern = "<span class=.genre.>\n.*?</span>"
+    return get_elements(html, pattern)
+
+
+def get_descriptions(html):
+    pattern = "<p class=.text-muted.>\n.*?</p>"
+    return get_elements(html, pattern)
+
+
+def get_images(html):
+    pattern = 'loadlate=".*?"'
     match_results = re.findall(pattern, html, re.IGNORECASE | re.MULTILINE)
-    elements = [re.sub("<.*?>", "", element) for element in [re.sub("<.*?\n>", "", element) for element in match_results]]
+    images = [re.sub('"', '', image) for image in [re.sub('loadlate="', '', image) for image in match_results]]
+    return images
+
+
+def get_ranks(html):
+    pattern = "<span class=.lister-item-index unbold text-primary.>.*?</span>"
+    return get_elements(html, pattern)
+
+
+def get_director(html):
+    pattern = "<p class=.text-muted.>\n.*?</p>\n.*?<p class=..>\n.*?Director:\n<a href=.*?\n>.*?</a>"
+    return get_elements(html, pattern)
+
+
+def get_stars(html):
+    pattern = "Stars:.*?</p>"
+    return get_elements(html, pattern)
+
+
+def get_classification_details(html):
+    pattern = " {4}<a href=./title/.*?<span class=.genre.>"
+    return get_elements(html, pattern)
+
+
+def get_rating_values(html):
+    pattern = " {4}<a href=./title/.*?<meta itemprop=.ratingValue. content=..*?. />.*?<span class=.rating-bg.>"
+    elements = get_elements(html, pattern)
+    rating_values = [re.sub("</a>", "", element) for element in [re.sub("content=.", "content=", element) for element in [re.sub("<span.*?>", "", element) for element in [re.sub("<meta itemprop=", "", element) for element in [re.sub("<span .*?itemprop=", "", element ) for element in [re.sub("</div>", "", element) for element in [re.sub("<div.*?>", "", element) for element in [re.sub("Rate this.*?<meta", "", element).strip() for element in [re.sub(". />", "", element) for element in elements]]]]]]]]]
+    return rating_values
+
+
+def get_metascores(html):
+    pattern = "(<a href=./title/.*?<div class=.lister-item-image float-left.>|<a href=./title/.*?lister-page-next next-page)"
+    match_results = re.findall(pattern, html, re.IGNORECASE | re.MULTILINE | re.DOTALL)
+    metascores = [re.sub("</span>.*?<div class=.lister-item-image float-left.>", "", element, flags=re.DOTALL) for element in [re.sub("</a>.*?class=.metascore", " \"metascore", element, flags=re.DOTALL) for element in [re.sub("<a href=./title/.*?<a href=./title/.*?>", "", element, flags = re.DOTALL) for element in match_results]]]
+    metascores = ["" if element.find('metascore') == -1 else element for element in metascores]
+    metascores = [re.sub(">", " ", element) for element in metascores]
+    return metascores
+
+def get_elements(html, pattern):
+    match_results = re.findall(pattern, html, re.IGNORECASE | re.MULTILINE | re.DOTALL)
+    elements = [re.sub("\n", "", element.strip()) for element in [re.sub("<.*?\n>", "", element) for element in match_results]]
     return elements
